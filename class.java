9a0e2d38330bb650792509fe7736e8a63d020e51
r4vemode <62845988+r4vemode@users.noreply.github.com>
2022-01-17 11:33:39 +0300

    Update class.java

--- class.java
+++ class.java
@@ -1,1 +1,84 @@
+public class Main {
+    public static void main(String[] args) {
+        BabushkaPhone BabushkaPhone = new BabushkaPhone();
+        SmartPhone SmartPhone = new SmartPhone();
+        System.out.println();
+    }
+}
 
+public class Phone {
+    public String serialNumber;
+    public String model;
+    public int weight;
+
+    public Phone(){
+
+        serialNumber = "+7-985-583-53-46";
+        model = "Nokia";
+        weight = 20;
+    }
+    public void receiveCall(String name){
+        System.out.println(" тебе звонит " + name);
+    }
+    public void getNumber(){
+        System.out.println(serialNumber);
+    }
+    void display() {
+        System.out.printf("Телефон: %s\nМодель: %s\nВес %d\n", serialNumber, model, weight);
+    }
+}
+public class BabushkaPhone extends Phone{
+    public int permission;
+    public String price;
+    public String camera;
+
+    public BabushkaPhone(){
+        int serialNumber = 112;
+    }
+
+    @Override
+    public void receiveCall(String name){
+        System.out.println("тебе звонит" + name);
+    }
+    public void getAllData() {
+        System.out.printf("permission: %d\nprice: %s\ncamera: %s\n", permission, price, camera);
+    }
+
+    public void getYouCall() {
+        System.out.println("вы звоните человеку");
+    }
+}
+
+public class SmartPhone extends Phone{
+    public int permission;
+    public String price;
+    public String camera;
+
+    public SmartPhone(){
+        super.model = "IPhone 11";
+        super.weight = 12;
+    }
+
+    public void sendMessage(){
+        System.out.println("вы получили сообщение!");
+    }
+    public void getAllData() {
+        System.out.printf("permission: %d\nprice: %s\ncamera: %s\n", permission, price, camera);
+    }
+
+    public void getVideo() {
+        System.out.println("вы снимаете видео!");
+    }
+  
+  public class CodeAnalyzer {
+
+   public static void analyzeClass(Object o) {
+       Class clazz = o.getClass();
+       System.out.println("Имя класса: " + clazz);
+       System.out.println("Поля класса: " + Arrays.toString(clazz.getDeclaredFields()));
+       System.out.println("Родительский класс: " + clazz.getSuperclass());
+       System.out.println("Методы класса: " +  Arrays.toString(clazz.getDeclaredMethods()));
+       System.out.println("Конструкторы класса: " + Arrays.toString(clazz.getConstructors()));
+   }
+  
+}
